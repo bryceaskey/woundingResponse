99b10e694c409e2874706730a32b83bf2ac53d88
Bryce Askey <bryceaskey@gmail.com>
2019-12-16 18:32:54 -0500

    Created aStar method for quantifying pinking

---
+++ code/aStarMethod.R
@@ -0,0 +1,21 @@
+# Install and load necessary packages
+packageList <- c("OpenImageR", "ggplot2")
+newPackages <- packageList[!(packageList %in% installed.packages()[,"Package"])]
+if(length(newPackages) > 0){
+  install.packages(newPackages)
+}
+library(OpenImageR)
+library(ggplot2)
+
+# Set main path
+mainPath <- "C:/Users/bca08_000/Documents/woundingResponse"
+if(getwd() != mainPath){
+  setwd(mainPath)
+}
+
+# Set path to processed images
+imagePath <- "C:/Users/bca08_000/Documents/woundingResponse/data/processedImages"
+
+# Load necessary functions
+source("code/aStarMean.R")
+
